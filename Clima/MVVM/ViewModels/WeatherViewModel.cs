bda83bb26f4f44e0d189b1701279a877778c38c9
Marco Alonso Rodriguez <mrodriguez@epica.solutions>
2024-02-23 09:13:36 -0600

    Getting Data from endpoint and desserializer info based on DataModel

--- Clima/MVVM/ViewModels/WeatherViewModel.cs
+++ Clima/MVVM/ViewModels/WeatherViewModel.cs
@@ -1,16 +1,45 @@
 ﻿using System;
+using System.Text.Json;
 using System.Windows.Input;
+using MauiWeather.MVVM.Models;
 
 namespace Clima.MVVM.ViewModels
 {
 	public class WeatherViewModel
 	{
+        public WeatherData WeatherData { get; set; }
+        private HttpClient client;
+
+        // Constructor
+        public WeatherViewModel()
+        {
+            client = new HttpClient();
+        }
+
         public ICommand SearchCommand =>
             new Command(async (searchText) =>
             {
                 var location = await GetCoordinatesAsync(searchText.ToString());
                 Console.WriteLine("location: " + location);
+                await GetWeather(location);
             });
+
+        private async Task GetWeather(Location location)
+        {
+            var url =
+                    $"https://api.open-meteo.com/v1/forecast?latitude={location.Latitude}&longitude={location.Longitude}&daily=weathercode,temperature_2m_max,temperature_2m_min&current_weather=true&timezone=America%2FChicago";
+            var response = await client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                using (var responseStream = await response.Content.ReadAsStreamAsync())
+                {
+                    var data = await JsonSerializer.DeserializeAsync<WeatherData>(responseStream);
+                    WeatherData = data;
+                    Console.WriteLine("temp: " + data.current_weather.temperature);
+                }
+            }
+        }
 
         // Recibe una cadena
         private async Task<Location> GetCoordinatesAsync(string address)
