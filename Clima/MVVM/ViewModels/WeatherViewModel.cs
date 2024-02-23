63eb5fe28ae3030272376cef45b93c97da626581
Marco Alonso Rodriguez <mrodriguez@epica.solutions>
2024-02-23 08:50:59 -0600

    Geolocation getCoordinates added

--- Clima/MVVM/ViewModels/WeatherViewModel.cs
+++ Clima/MVVM/ViewModels/WeatherViewModel.cs
@@ -1,11 +1,30 @@
 ﻿using System;
+using System.Windows.Input;
+
 namespace Clima.MVVM.ViewModels
 {
 	public class WeatherViewModel
 	{
-		public WeatherViewModel()
-		{
-		}
-	}
+        public ICommand SearchCommand =>
+            new Command(async (searchText) =>
+            {
+                var location = await GetCoordinatesAsync(searchText.ToString());
+                Console.WriteLine("location: " + location);
+            });
+
+        // Recibe una cadena
+        private async Task<Location> GetCoordinatesAsync(string address)
+        {
+            IEnumerable<Location> locations = await Geocoding
+                 .Default.GetLocationsAsync(address);
+
+            Location location = locations?.FirstOrDefault();
+
+            if (location != null)
+                Console
+                     .WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+            return location;
+        }
+    }
 }
 
